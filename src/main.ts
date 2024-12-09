60b6ade684892bdf26ae60c6abdd4b378973a763
SofienRogue <sofien.abidi@digitalroguewave.com>
2024-12-09 10:56:03 +0100

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -35,8 +35,8 @@ const whitelist = [
   'http://127.0.0.1:5001',
   'http://147.79.117.125:5000',
   'http://147.79.117.125:5001',
-  'https://api-tachkila.genydev.com/',
-  'https://dashboard-tachkila.genydev.com/',
+  'https://api-tachkila.genydev.com',
+  'https://api-tachkila.genydev.com',
 ];
 
 async function bootstrap() {
