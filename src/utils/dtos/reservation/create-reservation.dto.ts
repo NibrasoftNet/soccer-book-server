f929a954ecbf0372dbcb930ceb99644c10bae13e
SofienRogue <sofien.abidi@digitalroguewave.com>
2025-01-18 14:29:29 +0100

    fix reservation roles

--- src/utils/dtos/reservation/create-reservation.dto.ts
+++ src/utils/dtos/reservation/create-reservation.dto.ts
@@ -1,16 +1,10 @@
 import { IsDateString, IsNotEmpty, IsString, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
 
 export class CreateReservationDto {
   @ApiProperty()
   @IsNotEmpty()
   @IsDateString()
-  @Transform(({ value }) => {
-    const date = new Date(value);
-    date.setHours(0, 0, 0, 0); // Set hours, minutes, seconds, and milliseconds to zero
-    return date;
-  })
   day: Date;
 
   @ApiProperty()
